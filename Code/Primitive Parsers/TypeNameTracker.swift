dc7cb66bf2dd5d941534aff018415027098edf72
Zachary Drayer <zacharydrayer@gmail.com>
2023-06-25 18:39:13 -0700

    Fix tuple type delcaration handling

--- Code/Primitive Parsers/TypeNameTracker.swift
+++ Code/Primitive Parsers/TypeNameTracker.swift
@@ -4,23 +4,71 @@ import SwiftSyntax
 class TypeNameTracker: SyntaxVisitor, AnyTypeParser {
 	var value = ""
 	var isInTuple = false
-	
+	var isInFunction = false
+	var indexBeforeReturnStatementBegan: String.Index? = nil
+
 	required init() {
 		super.init(viewMode: .sourceAccurate)
 	}
-	
-	override func visit(_ node: TupleTypeElementSyntax) -> SyntaxVisitorContinueKind {
-		isInTuple = true
+
+	override func visit(_ node: TupleTypeSyntax) -> SyntaxVisitorContinueKind {
+		value += "("
 		return super.visit(node)
 	}
-	
+
+	override func visitPost(_ node: TupleTypeSyntax) {
+		value += ")"
+		return super.visitPost(node)
+	}
+
+	override func visit(_ node: TupleTypeElementListSyntax) -> SyntaxVisitorContinueKind {
+		if isInTuple {
+			value = value + ParseAnyType<TypeNameTracker>(node: node).run()
+			return .skipChildren
+		} else {
+			isInTuple = true
+		}
+		return super.visit(node)
+	}
+
+	override func visit(_ node: FunctionTypeSyntax) -> SyntaxVisitorContinueKind {
+		isInFunction = true
+		value = value.isEmpty ? "(" : value + ", ("
+		return super.visit(node)
+	}
+
+	override func visitPost(_ node: FunctionTypeSyntax) {
+		isInFunction = false
+
+		if let indexBeforeReturnStatementBegan {
+			value.insert(")", at: indexBeforeReturnStatementBegan)
+		}
+		return super.visitPost(node)
+	}
+
 	override func visit(_ node: MemberTypeIdentifierSyntax) -> SyntaxVisitorContinueKind {
-		value = value.isEmpty ? node.name.text : node.name.text + joiner + value
+		if value.isEmpty || value.hasSuffix(" -> ") || value.hasSuffix("(") {
+			value += node.name.text
+		} else {
+			value += (joiner + node.name.text)
+		}
 		return super.visit(node)
 	}
 	
 	override func visit(_ node: SimpleTypeIdentifierSyntax) -> SyntaxVisitorContinueKind {
-		value = value.isEmpty ? node.name.text : node.name.text + joiner + value
+		if value.isEmpty || value.hasSuffix(" -> ") || value.hasSuffix("(") {
+			value += node.name.text
+		} else {
+			value += (joiner + node.name.text)
+		}
+		return super.visit(node)
+	}
+
+	override func visit(_ node: ReturnClauseSyntax) -> SyntaxVisitorContinueKind {
+		if isInFunction {
+			indexBeforeReturnStatementBegan = value.endIndex
+			value = value + " -> "
+		}
 		return super.visit(node)
 	}
 }
