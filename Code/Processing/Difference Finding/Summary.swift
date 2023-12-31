c5c5b48b7f3a14aacb859c45158a376642902cd7
Zachary Drayer <zacharydrayer@gmail.com>
2023-12-31 15:47:58 -0800

    concurrent performs to process each framework, not each platform

--- Code/Processing/Difference Finding/Summary.swift
+++ Code/Processing/Difference Finding/Summary.swift
@@ -33,30 +33,26 @@ extension Summary {
 
 	static func createSummary(for path: String, typePrefixesToRemove: Set<String>, progress: Bool) -> Summary {
 		var results = Summary()
-		let all = SwiftmoduleFinder(app: path).run()
-		let keys = all.keys
-
-		let typePrefixesToRemoveWithQualifier = Set(typePrefixesToRemove.map { $0 + "." })
-
-		let lock = OSAllocatedUnfairLock()
-		DispatchQueue.concurrentPerform(iterations: keys.count) { i in
-			let platform = keys[all.keys.index(all.keys.startIndex, offsetBy: i)]
-			let architectureToModules = all[platform]!
+		let qualifiedTypePrefixesToRemove = Set(typePrefixesToRemove.map { $0 + "." })
+		for (platform, architectureToModules) in SwiftmoduleFinder(app: path).run() {
 			if progress { print(platform) }
 
 			var platformSDKs = [SwiftmoduleFinder.Architecture: Set<Framework>]()
 			for (architecture, moduleList) in architectureToModules {
 				if progress { print(architecture) }
 
+				let lock = OSAllocatedUnfairLock()
 				var architectureFrameworks = Set<Framework>()
-				for i in 0 ..< moduleList.count {
+				DispatchQueue.concurrentPerform(iterations: moduleList.count) { i in
 					let module = moduleList[i]
 					if progress { print(module.absoluteString) }
 
 					let path = module.absoluteString.replacingOccurrences(of: "file://", with: "")
-					let framework = ParseSwiftmodule(path: path, typePrefixesToRemove: typePrefixesToRemoveWithQualifier).run()
+					let framework = ParseSwiftmodule(path: path, typePrefixesToRemove: qualifiedTypePrefixesToRemove).run()
 
+					lock.lock()
 					architectureFrameworks.insert(framework)
+					lock.unlock()
 				}
 
 				if !architectureFrameworks.isEmpty {
@@ -65,9 +61,7 @@ extension Summary {
 			}
 
 			if !platformSDKs.isEmpty {
-				lock.lock()
 				results[platform] = platformSDKs
-				lock.unlock()
 			}
 		}
 
