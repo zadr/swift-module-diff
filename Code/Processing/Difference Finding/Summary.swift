75f380a13dd0a13732f0abb09f22524e86fef726
Zachary Drayer <zacharydrayer@gmail.com>
2023-12-31 17:28:49 -0800

    build a flat list of platform/architectures/modules and then run over it concurrently

--- Code/Processing/Difference Finding/Summary.swift
+++ Code/Processing/Difference Finding/Summary.swift
@@ -31,38 +31,44 @@ extension Summary {
 		return results
 	}
 
+	struct AllOfIt {
+		let platform: SwiftmoduleFinder.Platform
+		let architecture: SwiftmoduleFinder.Architecture
+		let url: URL
+	}
+
 	static func createSummary(for path: String, typePrefixesToRemove: [String], progress: Bool) -> Summary {
 		var results = Summary()
 		let qualifiedTypePrefixesToRemove = typePrefixesToRemove.map { $0 + "." }
+
+		var everything = [AllOfIt]()
 		for (platform, architectureToModules) in SwiftmoduleFinder(app: path).run() {
-			if progress { print(platform) }
+			results[platform] = [SwiftmoduleFinder.Architecture: Set<Framework>]()
 
-			var platformSDKs = [SwiftmoduleFinder.Architecture: Set<Framework>]()
 			for (architecture, moduleList) in architectureToModules {
-				if progress { print(architecture) }
+				results[platform]![architecture] = Set<Framework>()
 
-				let lock = OSAllocatedUnfairLock()
-				var architectureFrameworks = Set<Framework>()
-				DispatchQueue.concurrentPerform(iterations: moduleList.count) { i in
-					let module = moduleList[i]
-					if progress { print(module.absoluteString) }
-
-					let path = module.absoluteString.replacingOccurrences(of: "file://", with: "")
-					let framework = ParseSwiftmodule(path: path, typePrefixesToRemove: qualifiedTypePrefixesToRemove).run()
-
-					lock.lock()
-					architectureFrameworks.insert(framework)
-					lock.unlock()
-				}
-
-				if !architectureFrameworks.isEmpty {
-					platformSDKs[architecture] = architectureFrameworks
+				for module in moduleList {
+					everything.append(AllOfIt(platform: platform, architecture: architecture, url: module))
 				}
 			}
+		}
 
-			if !platformSDKs.isEmpty {
-				results[platform] = platformSDKs
-			}
+		let lock = OSAllocatedUnfairLock()
+		DispatchQueue.concurrentPerform(iterations: everything.count) { i in
+			let it = everything[i]
+			if progress { print("\(it.platform) \(it.architecture) \(it.url)") }
+
+			let path = it.url.absoluteString.replacingOccurrences(of: "file://", with: "")
+			let framework = ParseSwiftmodule(path: path, typePrefixesToRemove: qualifiedTypePrefixesToRemove).run()
+
+			lock.lock()
+			var p = results[it.platform]
+			var a = p?[it.architecture]
+			a?.insert(framework)
+			p?[it.architecture] = a
+			results[it.platform] = p
+			lock.unlock()
 		}
 
 		return results
