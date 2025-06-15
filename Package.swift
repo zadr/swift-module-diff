b494a341524cfe8975196d5dffa0c295fd28acfe
Zachary Drayer <zacharydrayer@gmail.com>
2025-06-14 20:31:26 -0400

    Update swift-syntax and swift-argument-parser

--- Package.swift
+++ Package.swift
@@ -6,15 +6,15 @@ let package = Package(
     name: "swift-module-diff",
     platforms: [.macOS(.v14)],
 	dependencies: [
-	    .package(url: "https://github.com/apple/swift-argument-parser.git", .upToNextMajor(from: "1.2.0")),
-		.package(url: "https://github.com/apple/swift-syntax.git", branch: "509.0.2"),
-		.package(url: "https://github.com/Kitura/swift-html-entities.git", from: "3.0.0"),
+	    .package(url: "https://github.com/apple/swift-argument-parser.git", .upToNextMajor(from: "1.3.0")),
+	    .package(url: "https://github.com/swiftlang/swift-syntax.git", branch: "600.0.1"),
+	    .package(url: "https://github.com/Kitura/swift-html-entities.git", from: "3.0.0"),
 	],
     targets: [
         .executableTarget(
             name: "swift-module-diff",
 			dependencies: [
-			    .product(name: "ArgumentParser", package: "swift-argument-parser"),
+				.product(name: "ArgumentParser", package: "swift-argument-parser"),
 				.product(name: "SwiftParser", package: "swift-syntax"),
 				.product(name: "SwiftSyntax", package: "swift-syntax"),
 				.product(name: "HTMLEntities", package: "swift-html-entities")
